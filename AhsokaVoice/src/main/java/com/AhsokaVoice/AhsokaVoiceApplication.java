d6f7d1ff8697cbceacea15b57c9d18218507a419
enviap <enviap@envia-p>
2024-08-11 19:25:28 -0600

    archivos iniciales

---
+++ AhsokaVoice/src/main/java/com/AhsokaVoice/AhsokaVoiceApplication.java
@@ -0,0 +1,13 @@
+package com.AhsokaVoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhsokaVoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhsokaVoiceApplication.class, args);
+	}
+
+}
---
+++ AhsokaVoice/src/test/java/com/AhsokaVoice/AhsokaVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AhsokaVoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhsokaVoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
